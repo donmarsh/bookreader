7e26ce8d94ebdc080d476c6a97222339debd099b
Marshall Musyimi Mutiso <marshallmusyimi@gmail.com>
2026-07-16 17:41:30 +0300

    Updated to fix display issues in the reader.

--- app/src/main/java/org/marshsoft/bookreader/ui/navigation/NavGraph.kt
+++ app/src/main/java/org/marshsoft/bookreader/ui/navigation/NavGraph.kt
@@ -1,8 +1,5 @@
 package org.marshsoft.bookreader.ui.navigation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
